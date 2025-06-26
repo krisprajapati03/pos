e345cc9d344d0d57fe1acacf91ecbdfc718db99d
krisprajapati03 <krisprajapati99@gmail.com>
2025-06-26 18:38:43 +0530

    refactor shop model and DAO for improved structure and readability

--- backend/src/models/shop.model.js
+++ backend/src/models/shop.model.js
@@ -1,15 +1,17 @@
-// Shop Model
 import mongoose from "mongoose";
+
 const shopSchema = new mongoose.Schema({
-  name: { type: String },
+  name: String,
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  contactNumber: { type: String },
+  contactNumber: String,
   GSTIN: String,
   address: String,
   logoURL: String,
   licenseStatus: String,
   licenseExpiryDate: Date,
-  planType: String,
+  planType: { type: String, enum: ["basic","premium"], default: "basic" },
+  planExpiry: Date,
+  isActive: { type: Boolean, default: true }
 }, { timestamps: true });
 
 export const Shop = mongoose.model("Shop", shopSchema);
